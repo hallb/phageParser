5d348631782dbd015c1f99614cce328cdc727b8b
Ben Hall <hallb@intware.com>
2017-12-14 09:54:17 -0500

    WIP: Setup.

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+sudo apt install python3 python3-venv postgresql postgresql-server-dev-9.5
+python3 -m venv venv
+# cp scripts/pip.conf venv/pip.conf
+
+echo 'source venv/bin/activate'
+
---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+pip install --upgrade pip setuptools
+## --trusted-host 
+# pip install --cert /usr/local/share/ca-certificates/cert_Intelliware_SSL_Inspection.pem
+pip install -r requirements.txt
+
