0f0231d25245e33989be4d6d5b04708363e353fa
Ben Hall <ben.hall@intelliware.com>
2017-12-15 09:43:45 -0500

    Add jupyter to setup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,5 @@
 pip install --upgrade pip setuptools
+pip install jupyter
 ## --trusted-host 
 # pip install --cert /usr/local/share/ca-certificates/cert_Intelliware_SSL_Inspection.pem
 pip install -r requirements.txt
