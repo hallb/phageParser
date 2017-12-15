c371312f896936f761c017b44781bd8b68c7a940
Ben Hall <ben.hall@intelliware.com>
2017-12-14 20:22:07 -0500

    Add dependency python3-dev.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-sudo apt install python3 python3-venv postgresql postgresql-server-dev-9.5
+sudo apt install python3 python3-dev python3-venv postgresql postgresql-server-dev-9.5
 python3 -m venv venv
 # cp scripts/pip.conf venv/pip.conf
 
