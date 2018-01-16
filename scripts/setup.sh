d3b737cee5df4e21002f1cda1aa23334481c50ad
Ben Hall <ben.hall@intelliware.com>
2018-01-15 21:51:53 -0500

    Add some docs regarding dev scripts.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env sh
+# Run the following, having activated a virtualenv
 pip install --upgrade pip setuptools
 pip install jupyter
-## --trusted-host 
-# pip install --cert /usr/local/share/ca-certificates/cert_Intelliware_SSL_Inspection.pem
 pip install -r requirements.txt
-
