27bce7e13ba2ba7011afd59754175deccb9e4efd
Ben Hall <ben.hall@intelliware.com>
2018-01-15 21:31:47 -0500

    Clarify instruction.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 sudo apt install python3 python3-dev python3-venv postgresql postgresql-server-dev-9.5
 python3 -m venv venv
+
 # cp scripts/pip.conf venv/pip.conf
 
-echo 'source venv/bin/activate'
+echo 'Next Step: source venv/bin/activate'
 
